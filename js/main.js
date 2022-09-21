493343ab265382cf982b2aef8db7c2d718232f70
Maksiz03 <92305927+Maksiz03@users.noreply.github.com>
2022-09-21 19:55:00 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function randomNumbers(min, max)
 {
-  if(min >=0 && min<=max)
-  {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  else
-  return "range!!!";
+    if(min >=0 && min<=max)
+    {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    else
+      return 'range!!!';
 }
 
 
